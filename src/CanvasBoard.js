57f09b9be2ca3cb0474b7a29035d50350217f886
Sourav Shaw <sourav@unacademy.com>
2019-02-23 19:26:30 +0530

    Changes in config

---
+++ src/CanvasBoard.js
@@ -0,0 +1,10 @@
+class CanvasBoard {
+    constructor(canvas) {
+        this.toString();
+    }
+
+    toString() {
+        console.log('fsadsgfdgf');
+    }
+}
+export default CanvasBoard;
--- src/listener.js
+++ src/listener.js
@@ -1,15 +1,8 @@
+import CanvasBoard from "./CanvasBoard";
+
 export function initCanvas(){
     console.log("--init canvas");
-    let canvas = document.querySelector("#canvas");
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    let context = canvas.getContext("2d");
-    listenToMouseEvents(canvas, context);
-}
-
-function listenToMouseEvents(canvas){
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mouseup', () => isDrawing = false);
-    canvas.addEventListener('mouseout', () => isDrawing = false);
+    let canvas = new CanvasBoard();
+    console.log("--canvas");
 }
 
